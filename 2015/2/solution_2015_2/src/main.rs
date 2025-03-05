bbf6377327e130e1483d6f7529829a46842964af
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-05 15:28:54 +0200

    2015_2: Get Box from String

--- 2015/2/solution_2015_2/src/main.rs
+++ 2015/2/solution_2015_2/src/main.rs
@@ -1,4 +1,4 @@
-type Feet = i32;
+type Feet = u32;
 
 struct Box {
     l: Feet,
@@ -7,6 +7,20 @@ struct Box {
 }
 
 impl Box {
+    fn from_string(string: String) -> Result<Self, String> {
+        let dimensions_vec = convert_vec_string_to_u32(split_string_by_delim(string));
+
+        if dimensions_vec.is_err() {
+            return Err(String::from("Malformed input"));
+        }
+
+        if let [l, w, h] = dimensions_vec.unwrap()[..] {
+            return Ok(Box { l, w, h });
+        }
+
+        Err(String::from("Malformed input"))
+    }
+
     fn get_wrapping_paper_needed(&self) -> Feet {
         let mut wrapping_paper = 0;
 
@@ -25,9 +39,64 @@ impl Box {
 }
 
 fn main() {
-    let box1 = Box { l: 2, w: 3, h: 4 };
-    assert_eq!(58, box1.get_wrapping_paper_needed());
+    let box1_input = String::from("2x3x4");
 
-    let box2 = Box { l: 1, w: 1, h: 10 };
-    assert_eq!(43, box2.get_wrapping_paper_needed());
+    let some_box = Box::from_string(box1_input);
+
+    let paper = match some_box {
+        Ok(res) => res.get_wrapping_paper_needed(),
+        Err(err) => {
+            println!("{err}");
+            0
+        }
+    };
+
+    println!("{paper}");
+}
+
+fn split_string_by_delim(input: String) -> Vec<String> {
+    let mut delims: Vec<usize> = vec![];
+
+    for (i, v) in input.chars().enumerate() {
+        if v == 'x' {
+            delims.push(i);
+        }
+    }
+
+    let mut last_delim: usize = 0;
+    let mut segments: Vec<String> = vec![];
+    for i in delims {
+        let slice = &input[last_delim..i];
+
+        segments.push(String::from(slice));
+        //Skip one char to the right to avoid the "x"
+        last_delim = i + 1;
+    }
+    if last_delim < input.len() {
+        let slice = &input[last_delim..input.len()];
+        segments.push(String::from(slice));
+    }
+
+    segments
+}
+
+fn convert_vec_string_to_u32(input: Vec<String>) -> Result<Vec<u32>, bool> {
+    let mut result = vec![];
+
+    for i in input {
+        if !string_slice_is_num(&i) {
+            return Err(false);
+        }
+        result.push(string_to_int(&i));
+    }
+
+    Ok(result)
+}
+
+fn string_slice_is_num(slice: &str) -> bool {
+    !slice.parse::<u32>().is_err()
+}
+
+fn string_to_int(slice: &str) -> u32 {
+    slice.parse::<u32>().unwrap()
 }
