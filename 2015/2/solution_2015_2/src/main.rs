d163f9c977ff7f01c35b453013fbf94ad52a1011
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-25 11:23:33 +0200

    2015_2: Finalize

--- 2015/2/solution_2015_2/src/main.rs
+++ 2015/2/solution_2015_2/src/main.rs
@@ -12,15 +12,17 @@ impl Box {
     fn from_string(string: &str) -> Result<Self, String> {
         let vec_slice = split_string_by_delim(string, 'x');
 
-        //TODO: There has to be a better way to do this
         let dimensions_vec = vec_string_to_u32(vec_slice);
 
-        if dimensions_vec.is_err() {
-            return Err(String::from("Malformed input"));
-        }
-
-        if let [l, w, h] = dimensions_vec.unwrap()[..] {
-            return Ok(Box { l, w, h });
+        match dimensions_vec {
+            Ok(vec) => {
+                if let [l, w, h] = vec[..] {
+                    return Ok(Box { l, w, h });
+                }
+            }
+            Err(err) => {
+                return Err(err);
+            }
         }
 
         Err(String::from("Malformed input"))
@@ -29,23 +31,64 @@ impl Box {
     fn get_wrapping_paper_needed(&self) -> Feet {
         let mut wrapping_paper = 0;
 
-        let side_lw = self.l * self.w;
-        let side_wh = self.w * self.h;
-        let side_lh = self.l * self.h;
-
-        let sides = [side_lw, side_wh, side_lh];
-        let smallest_side = sides.iter().min().unwrap();
-
-        wrapping_paper += side_lw * 2 + side_wh * 2 + side_lh * 2;
-        wrapping_paper += smallest_side;
+        wrapping_paper += self.get_box_area();
+        wrapping_paper += self.get_smallest_side_area();
 
         wrapping_paper
+    }
+
+    fn get_smallest_side_area(&self) -> Feet {
+        let side_lw = BoxSide(self.l, self.w).area();
+        let side_wh = BoxSide(self.w, self.h).area();
+        let side_lh = BoxSide(self.l, self.h).area();
+
+        let sides = [side_lw, side_wh, side_lh];
+        sides.iter().min().unwrap().clone()
+    }
+
+    fn get_box_area(&self) -> Feet {
+        let side_lw = BoxSide(self.l, self.w).area();
+        let side_wh = BoxSide(self.w, self.h).area();
+        let side_lh = BoxSide(self.l, self.h).area();
+
+        side_lw * 2 + side_wh * 2 + side_lh * 2
+    }
+
+    fn get_box_volume(&self) -> Feet {
+        self.l * self.w * self.h
+    }
+
+    fn get_ribbon_needed(&self) -> Feet {
+        let mut ribbon: Feet = 0;
+
+        let side_lw = BoxSide(self.l, self.w).perimeter();
+        let side_wh = BoxSide(self.w, self.h).perimeter();
+        let side_lh = BoxSide(self.l, self.h).perimeter();
+
+        let sides = [side_lw, side_wh, side_lh];
+        ribbon += sides.iter().min().unwrap();
+        ribbon += self.get_box_volume();
+
+        ribbon
+    }
+}
+
+struct BoxSide(Feet, Feet);
+
+impl BoxSide {
+    fn perimeter(&self) -> Feet {
+        2 * self.0 + 2 * self.1
+    }
+
+    fn area(&self) -> Feet {
+        self.0 * self.1
     }
 }
 
 fn main() {
     let file_path = "presents_dimensions.txt";
     let mut paper_total: Feet = 0;
+    let mut ribbon_total: Feet = 0;
 
     match fs::read_to_string(file_path) {
         Ok(presents) => {
@@ -54,15 +97,18 @@ fn main() {
 
                 let some_box = Box::from_string(box1_input);
 
-                paper_total += match some_box {
-                    Ok(res) => res.get_wrapping_paper_needed(),
+                match some_box {
+                    Ok(res) => {
+                        paper_total += res.get_wrapping_paper_needed();
+                        ribbon_total += res.get_ribbon_needed();
+                    }
                     Err(err) => {
                         println!("{err}");
-                        0
                     }
                 };
             }
-            println!("{paper_total}");
+            println!("Paper needed: {paper_total}");
+            println!("Ribbon needed: {ribbon_total}");
         }
         Err(e) => {
             println!("Error reading file at {file_path}. Error {e}");
@@ -71,48 +117,18 @@ fn main() {
 }
 
 fn split_string_by_delim(input: &str, delim: char) -> Vec<String> {
-    let mut delims: Vec<usize> = vec![];
-
-    for (i, v) in input.chars().enumerate() {
-        if v == delim {
-            delims.push(i);
-        }
-    }
-
-    let mut pointer: usize = 0;
-    let mut segments: Vec<String> = vec![];
-    for i in delims {
-        let slice = &input[pointer..i];
-
-        segments.push(String::from(slice));
-        //Skip one char to the right to avoid the delimiter
-        pointer = i + 1;
-    }
-    if pointer < input.len() {
-        let slice = &input[pointer..input.len()];
-        segments.push(String::from(slice));
-    }
-
-    segments
+    input.split(delim).map(String::from).collect()
 }
 
-fn vec_string_to_u32(input: Vec<String>) -> Result<Vec<u32>, bool> {
+fn vec_string_to_u32(input: Vec<String>) -> Result<Vec<u32>, String> {
     let mut result = vec![];
 
     for i in input {
-        if !string_slice_is_num(&i) {
-            return Err(false);
+        match i.parse::<u32>() {
+            Ok(num) => result.push(num),
+            Err(_) => return Err(format!("Failed to parse '{}' as u32", i)),
         }
-        result.push(string_to_int(&i));
     }
 
     Ok(result)
-}
-
-fn string_slice_is_num(slice: &str) -> bool {
-    !slice.parse::<u32>().is_err()
-}
-
-fn string_to_int(slice: &str) -> u32 {
-    slice.parse::<u32>().unwrap()
 }
