93846f5e1455a9d0b367190d0d150328edf623d8
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-26 09:30:17 +0200

    Add row prepend + rename coord to cursor

--- 2015/solution_3/src/main.rs
+++ 2015/solution_3/src/main.rs
@@ -5,8 +5,23 @@ struct Row {
 
 #[derive(Debug)]
 struct Grid {
-    coordinate: (u32, u32),
+    cursor: (u32, u32),
     rows: Vec<Row>,
+}
+
+impl Row {
+    fn prepend(&mut self) {
+        let mut new_cells: Vec<u32> = vec![0];
+        new_cells.append(&mut self.cells);
+        self.cells = new_cells;
+    }
+}
+
+#[test]
+fn test_prepend() {
+    let mut row = Row { cells: vec![1, 2] };
+    row.prepend();
+    assert_eq!(vec![0, 1, 2], row.cells);
 }
 
 impl Grid {
@@ -14,7 +29,7 @@ impl Grid {
         let mut row = Row { cells: vec![0] };
         let mut grid = Grid {
             rows: vec![row],
-            coordinate: (0, 0),
+            cursor: (0, 0),
         };
 
         grid
