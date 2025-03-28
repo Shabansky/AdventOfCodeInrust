ea56a6d4ef11ca5c4d7ff2f306185e83bfe69563
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-03-28 07:25:27 +0200

    2015_3: Finish grid growth methods

--- 2015/solution_3/src/main.rs
+++ 2015/solution_3/src/main.rs
@@ -3,12 +3,6 @@ struct Row {
     cells: Vec<u32>,
 }
 
-#[derive(Debug)]
-struct Grid {
-    cursor: (u32, u32),
-    rows: Vec<Row>,
-}
-
 impl Row {
     fn prepend(&mut self) {
         let mut new_cells: Vec<u32> = vec![0];
@@ -35,19 +29,73 @@ fn test_append() {
     assert_eq!(vec![1, 2, 0], row.cells);
 }
 
+#[derive(Debug)]
+struct Grid {
+    cursor: (u32, u32),
+    rows: Vec<Row>,
+    width: u32,
+    height: u32,
+}
+
 impl Grid {
     fn new() -> Grid {
-        let mut row = Row { cells: vec![0] };
-        let mut grid = Grid {
-            rows: vec![row],
+        let row = Row { cells: vec![0] };
+        let grid = Grid {
             cursor: (0, 0),
+            rows: vec![row],
+            width: 1,
+            height: 0,
         };
 
         grid
+    }
+
+    fn grow_left(&mut self) {
+        for row in self.rows.iter_mut() {
+            row.prepend();
+        }
+
+        self.width += 1;
+    }
+
+    fn grow_right(&mut self) {
+        for row in self.rows.iter_mut() {
+            row.append();
+        }
+
+        self.width += 1;
+    }
+
+    fn grow_up(&mut self) {
+        let mut new_rows = vec![self.new_row()];
+        new_rows.append(&mut self.rows);
+        self.rows = new_rows;
+
+        self.height += 1;
+    }
+
+    fn grow_down(&mut self) {
+        self.rows.push(self.new_row());
+
+        self.height += 1;
+    }
+
+    fn new_row(&self) -> Row {
+        Row {
+            cells: vec![0; self.width as usize],
+        }
     }
 }
 
 fn main() {
     let mut grid = Grid::new();
-    println!("{:?}", grid);
+    println!("{:#?}", grid);
+    grid.grow_left();
+    println!("{:#?}", grid);
+    grid.grow_right();
+    println!("{:#?}", grid);
+    grid.grow_up();
+    println!("{:#?}", grid);
+    grid.grow_down();
+    println!("{:#?}", grid);
 }
