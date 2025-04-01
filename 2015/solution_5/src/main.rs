90dbccf2ee2c1af09a368b3ee4380af923728c58
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-04-01 10:08:23 +0300

    2015_5: Add vowel checker function

--- 2015/solution_5/src/main.rs
+++ 2015/solution_5/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+fn has_num_of_vowels(text: &String, threshold: u32) -> bool {
+    let vowels = [b'a', b'e', b'i', b'o', b'u'];
+
+    let mut num_vowels = 0;
+
+    for v in text.as_bytes().iter() {
+        if vowels.contains(v) {
+            num_vowels += 1;
+        }
+    }
+
+    num_vowels >= threshold
+}
+
+#[test]
+fn test_has_num_of_vowels() {
+    let text = String::from("test");
+    assert_eq!(false, has_num_of_vowels(&text, 3));
+    let text = String::from("aaabcd");
+    assert_eq!(true, has_num_of_vowels(&text, 3));
+    let text = String::from("      ");
+    assert_eq!(false, has_num_of_vowels(&text, 3));
+    assert_eq!(true, has_num_of_vowels(&text, 0));
+    let text = String::from("(@S@DM(92da!!#X");
+    assert_eq!(true, has_num_of_vowels(&text, 1));
 }
