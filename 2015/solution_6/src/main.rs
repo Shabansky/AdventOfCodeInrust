f2cdcde597d345354a3c89fc688002bb2611049b
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-04-21 20:59:58 +0300

    2015_6: Init map and print it for fun :)

--- 2015/solution_6/src/main.rs
+++ 2015/solution_6/src/main.rs
@@ -1,3 +1,13 @@
+const SQUARE_SIDE: usize = 1000;
+
 fn main() {
-    println!("Hello, world!");
+    //Map allocated on heap via vec as a 1000x1000 on an array will exceed the stack memory limit.
+    let map = vec![vec![0; SQUARE_SIDE]; SQUARE_SIDE];
+
+    for row in &map {
+        for col in row {
+            print!("{col} ");
+        }
+        println!("");
+    }
 }
