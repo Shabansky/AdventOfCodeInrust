81904e589c43aa4c8dc4cb51bc919dc3bbd45112
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-05-13 20:36:31 +0300

    Add structs for each light

--- 2015/solution_6/src/main.rs
+++ 2015/solution_6/src/main.rs
@@ -1,13 +1,44 @@
-const SQUARE_SIDE: usize = 1000;
+use std::default::Default;
+
+const SQUARE_SIDE: usize = 10;
+
+#[derive(Clone, Debug)]
+enum LightState {
+    Lit,
+    Unlit,
+}
+#[derive(Clone, Debug)]
+struct Light {
+    state: LightState,
+}
+
+impl Light {
+    fn new() -> Self {
+        Light {
+            state: LightState::Unlit,
+        }
+    }
+}
+
+impl Default for Light {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 fn main() {
     //Map allocated on heap via vec as a 1000x1000 on an array will exceed the stack memory limit.
-    let map = vec![vec![0; SQUARE_SIDE]; SQUARE_SIDE];
+    let map = build_map();
+}
+
+fn build_map() -> Vec<Vec<Light>> {
+    let map = vec![vec![Light::default(); SQUARE_SIDE]; SQUARE_SIDE];
 
     for row in &map {
         for col in row {
-            print!("{col} ");
+            print!("{col:?} ");
         }
-        println!("");
     }
+
+    map
 }
