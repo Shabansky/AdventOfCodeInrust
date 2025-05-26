d369a168888ac5288365290f67df05b5eff1cbc6
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-05-26 18:08:52 +0300

    2015_7_FTR: Define gate structs

--- 2015/solution_7/src/main.rs
+++ 2015/solution_7/src/main.rs
@@ -4,11 +4,33 @@ struct Node {
     signal: Signal,
 }
 
+type NodePair = (Node, Node);
+
 struct Wire {
     id: String,
     signal: Signal,
     input: Node,
     output: Vec<Node>,
+}
+
+struct GateShift {
+    input: Node,
+    output: Node,
+}
+struct GateAnd {
+    input: NodePair,
+    signal: Signal,
+    output: Node,
+}
+struct GateOr {
+    input: NodePair,
+    signal: Signal,
+    output: Node,
+}
+struct GateNot {
+    input: NodePair,
+    signal: Signal,
+    output: Node,
 }
 fn main() {
     println!("Hello, world!");
