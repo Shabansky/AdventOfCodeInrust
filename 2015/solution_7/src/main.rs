0cd6c92b5bb8317270c16486df2dfc4fc60bc34d
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-05-28 11:51:42 +0300

    2015_7_FTR: Add circuit builder + basic wire register

--- 2015/solution_7/src/main.rs
+++ 2015/solution_7/src/main.rs
@@ -56,6 +56,61 @@ struct GateNot {
     signal: Signal,
     output: Node,
 }
+
+mod circuit {
+    use super::Wire;
+    use std::collections::HashMap;
+
+    #[derive(PartialEq, Debug)]
+    enum RegisterError {
+        WireAlreadyExists,
+    }
+    struct CircuitBuilder {
+        wire_register: HashMap<String, Wire>,
+    }
+
+    impl CircuitBuilder {
+        fn new() -> Self {
+            Self {
+                wire_register: HashMap::new(),
+            }
+        }
+
+        fn add_wire_to_register(&mut self, wire: Wire) -> Result<(), RegisterError> {
+            let wire_id = wire.id.clone();
+            if let Some(_) = self.wire_register.get(&wire_id) {
+                Err(RegisterError::WireAlreadyExists)
+            } else {
+                self.wire_register.insert(wire_id, wire);
+                Ok(())
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        #[test]
+        fn add_wire_updates_register() {
+            let mut builder = CircuitBuilder::new();
+
+            assert_eq!(0, builder.wire_register.len());
+            let _ = builder.add_wire_to_register(Wire::new("ab".to_string()));
+            assert_eq!(1, builder.wire_register.len());
+        }
+
+        #[test]
+        fn duplicate_wire_register_produces_error() {
+            let mut builder = CircuitBuilder::new();
+
+            let _ = builder.add_wire_to_register(Wire::new("ab".to_string()));
+
+            let error = builder.add_wire_to_register(Wire::new("ab".to_string()));
+            assert_eq!(RegisterError::WireAlreadyExists, error.unwrap_err());
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
